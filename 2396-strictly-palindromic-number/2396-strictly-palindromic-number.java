c116faaa282066d86d6e14b31b89dd49a9b9892e
ysagar11y <46919299+ysagar11y@users.noreply.github.com>
2022-09-04 17:39:11 +0530

    Time: 1 ms (45.45%), Space: 41.2 MB (18.18%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        for(int i=2;i<=n-2;i++){
+          if( !checkPalindrome(Integer.toString(n,i)) )   
+          return false;
+      }
+      return true;
+    }
+    
+    public boolean checkPalindrome(String s){
+        
+        for(int i=0;i<s.length()/2;i++){
+            
+            if( !(s.charAt(i) == s.charAt(s.length()-i-1)) )
+            return false;
+        }
+        return true;
     }
 }
