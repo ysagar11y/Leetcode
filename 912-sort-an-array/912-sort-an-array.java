bb461e79302403c14b1d8a346779043d50ea4f1e
ysagar11y <46919299+ysagar11y@users.noreply.github.com>
2022-09-06 11:27:46 +0530

    Time: 5 ms (98.48%), Space: 75.9 MB (13.00%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,6 +1,29 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+        int minv,maxv=minv=nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			if(nums[i]>maxv) {
+				maxv=nums[i];
+			}
+			if(nums[i]<minv) {
+				minv=nums[i];
+			}
+		}
+		int n=maxv-minv;
+		int[]arr2=new int[n+1];
+		for (int i = 0; i < nums.length; i++) {
+			
+				arr2[nums[i]-minv]++;
+		}
+		int k=0;
+		for (int i = 0; i < arr2.length;i++ ) {
+			
+			for (int j=0; j < arr2[i]; j++) {
+				
+				nums[k++]=minv+i;
+				
+			}
+		}
+		return nums;
     }
 }
