b395dc6079dda81589c4361275c6b2e504af5db2
ysagar11y <46919299+ysagar11y@users.noreply.github.com>
2022-09-14 07:07:55 +0530

    Time: 1 ms (38.58%), Space: 42.3 MB (80.41%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,19 +1,36 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
+        if (matrix.length == 0 || matrix[0].length == 0) 
             return false;
-        }
-        int m=matrix[0].length;
-        int n=matrix.length;
-        int i=0,j=m-1;
-        while(i<n&&j>=0){
-            if(matrix[i][j]==target)
-                return true;
-            if(matrix[i][j]<target)
-                i++;
-            else
-                j--;
-        }
+            int n=matrix.length;
+            int m=matrix[0].length;
+            int l=0;
+            int r=n*m-1;
+            
+            while(l<=r){
+                int mid=(l+(r-l)/2);
+                if(matrix[mid/m][mid%m]==target)
+                    return true;
+                else if(matrix[mid/m][mid%m]<target)
+                    l=mid+1;
+                else
+                    r=mid-1;
+            }
+            
         return false;
-    }
+        
+        }
 }
+
+// int m=matrix[0].length;
+        // int n=matrix.length;
+        // int i=0,j=m-1;
+        // while(i<n&&j>=0){
+        //     if(matrix[i][j]==target)
+        //         return true;
+        //     if(matrix[i][j]<target)
+        //         i++;
+        //     else
+        //         j--;
+        // }
+        // return false;
