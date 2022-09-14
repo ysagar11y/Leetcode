5e883c022f7d03fdc67e48175883dea7d3e7b182
ysagar11y <46919299+ysagar11y@users.noreply.github.com>
2022-09-14 08:14:39 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (94.61%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -9,28 +9,44 @@
  * }
  */
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy=new ListNode(0);
-        dummy.next=head;
-        ListNode cur=dummy;
-        
-        if (head == null) {
-            return null;
-        }
-        int len=0;
-        while(head!=null){
-            len++;
-            head=head.next;
-        }
-        len=len-n;
-        int temp=0;
-        while(cur.next!=null){
-           if(temp==len)
-                cur.next=cur.next.next;
-            else
-                cur=cur.next;
-            temp++;
-        }
-        return dummy.next;
+    public ListNode removeNthFromEnd(ListNode head, int n) {   
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        slow.next = head;
+    
+        //Move fast in front so that the gap between slow and fast becomes n
+        for(int i=1; i<=n+1; i++)   {
+            fast = fast.next;
+            }
+        //Move fast to the end, maintaining the gap
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+            }
+        //Skip the desired node
+        slow.next = slow.next.next;
+        return start.next;
     }
 }
+//         ListNode dummy=new ListNode(0);
+//         dummy.next=head;
+//         ListNode cur=dummy;
+        
+//         if (head == null) {
+//             return null;
+//         }
+//         int len=0;
+//         while(head!=null){
+//             len++;
+//             head=head.next;
+//         }
+//         len=len-n;
+//         int temp=0;
+//         while(cur.next!=null){
+//            if(temp==len)
+//                 cur.next=cur.next.next;
+//             else
+//                 cur=cur.next;
+//             temp++;
+//         }
+//         return dummy.next;
