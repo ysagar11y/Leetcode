407fd0ce13c5adc4b13b75abb29c3043440e7e26
ysagar11y <46919299+ysagar11y@users.noreply.github.com>
2022-09-15 07:32:40 +0530

    Time: 16 ms (23.83%), Space: 49.8 MB (54.92%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,17 +1,53 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list=new ArrayList<>();
-        Map<Integer,Integer> map =new HashMap<>();
-        int ch=nums.length/3;
+        int nums1=-1,nums2=-1,c1=0,c2=0,n=nums.length/3;
         for(int i:nums){
-            int cnt = map.getOrDefault(i, 0)+1;
-            map.put(i,cnt);
-            System.out.println(i+" "+cnt);
-            if(cnt>ch){
-                if(!list.contains(i))
-                list.add(i);
+            if(i==nums1) c1++;
+            else if(i==nums2) c2++;
+            else if(c1==0){
+                nums1=i;
+                c1=1;
+            }
+            else if(c2==0){
+                nums2=i;
+                c2=1;
+            }
+            else
+            {
+                c1--;
+                c2--;
             }
         }
-        return list;
+        
+        System.out.println(nums1+" "+nums2);
+        List ans=new ArrayList<>();
+        c1=0;
+        c2=0;
+        for(int i:nums){
+            if(i==nums1)
+                c1++;
+            else if(i==nums2)
+                c2++;
+        }
+        if(c1>n)
+            ans.add(nums1);
+        if(c2>n)
+            ans.add(nums2);
+        return ans;
     }
 }
+
+
+        // List<Integer> list=new ArrayList<>();
+        // Map<Integer,Integer> map =new HashMap<>();
+        // int ch=nums.length/3;
+        // for(int i:nums){
+        //     int cnt = map.getOrDefault(i, 0)+1;
+        //     map.put(i,cnt);
+        //     System.out.println(i+" "+cnt);
+        //     if(cnt>ch){
+        //         if(!list.contains(i))
+        //         list.add(i);
+        //     }
+        // }
+        // return list;
