686318f536a65a41aad3103fe338b7c2506786d2
ysagar11y <46919299+ysagar11y@users.noreply.github.com>
2022-09-21 07:37:59 +0530

    Time: 2 ms (79.79%), Space: 52.4 MB (47.36%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -5,7 +5,6 @@ class Solution {
         for (int i = 0; i < nums.length; ++i) {
             if (leftsum == sum - leftsum - nums[i]) return i;
             leftsum += nums[i];
-
         }
         return -1;
     }
