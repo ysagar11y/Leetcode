ef4374dbaf601070e6bd0ae8242bc4901028abc4
ysagar11y <46919299+ysagar11y@users.noreply.github.com>
2022-09-21 07:54:40 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (97.33%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,27 +1,21 @@
 class Solution {
-    List<List<Integer>> lists;
+    List<List<Integer>> lists = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        lists = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        solve(list,nums,0);
-        
+        permutation(list,nums,0);   
         return lists;
     }
-    
-    void solve(List<Integer> list,int[] nums,int curr)
+    void permutation(List<Integer> list,int[] nums,int curr)
     {
-        
         if(curr == nums.length)
         {
-            lists.add(new ArrayList(list));
+            lists.add(new ArrayList<>(list));
             return;
         }
-        
         list.add(nums[curr]);
-        solve(list,nums,curr+1);
+        permutation(list,nums,curr+1);
         list.remove(list.size() - 1);
-        solve(list,nums,curr+1);
-        
+        permutation(list,nums,curr+1);
     }
 }
 
